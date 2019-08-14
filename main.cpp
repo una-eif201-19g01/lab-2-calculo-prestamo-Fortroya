7a3e9f2c26621f61119022b6a85475713377acc7
Jose Miguel <53483639+Jose0696@users.noreply.github.com>
2019-08-14 00:25:58 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,16 @@
 #include "CalculoPrestamo.h"
 
 int main() {
+    CalculoPrestamo calculoPrestamo(3000);
+
+    std::string reporte;
+
+    reporte = calculoPrestamo.reporteCalculoPrestamo("1 A", "8.40%");
+
+    std::cout << "Calcular prestamo: " << std::endl;
+
+    std::cout << reporte << std::endl;
+
+
     return 0;
 }
