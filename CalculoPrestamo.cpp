496d7b414709bfdeb23b077799ef41b7543f3403
Jose Miguel <53483639+Jose0696@users.noreply.github.com>
2019-08-14 00:24:25 -0600

    Update CalculoPrestamo.cpp

--- CalculoPrestamo.cpp
+++ CalculoPrestamo.cpp
@@ -1,26 +1,78 @@
-//
-// Created by Maikol Guzman  on 2019-08-08.
-//
-
 #include <iostream>
 #include "CalculoPrestamo.h"
 
-std::string obtenerPrestamo(std::string porcentajeTXT) {
-}
-
 CalculoPrestamo::CalculoPrestamo() {
+    monto = 0;
 }
 
-CalculoPrestamo::CalculoPrestamo(int64_t monto) {
+CalculoPrestamo::CalculoPrestamo(int64_t monto) : monto(monto) {
+    this->monto = monto;
 }
-std::string reporteCalculoPrestamo(std::string tiempoTXT, std::string porcentajeTXT) {
+
+float CalculoPrestamo::obtenerPorcentaje(std::string porcentajeTXT) {
+    float porcentaje = 0;
+    int buscarPorcentaje = porcentajeTXT.find("%");
+
+    if (buscarPorcentaje > 0) {
+        porcentaje = std::stof(porcentajeTXT.substr(0, buscarPorcentaje));
+        if (porcentaje > 0) {
+            porcentaje = porcentaje / 100;
+
+        }
+    }
+    return porcentaje;
+}
+
+int CalculoPrestamo::calcularTiempoMeses(std::string tiempoTXT) {
+    int meses = 0;
+    int ano = 0;
+    int buscarAno = tiempoTXT.find("A");
+    int buscarMes = tiempoTXT.find("M");
+
+    if (buscarAno > 0) {
+        ano = stoi(tiempoTXT.substr(0, buscarAno));
+        if (ano > 0) {
+            meses = ano + 12;
+
+        }
+
+    } else if (buscarMes > 0) {
+        meses = stoi(tiempoTXT.substr(0, buscarMes));
+
+    }
+    return meses;
+}
+
+float CalculoPrestamo::obtenerInteresMensual(float balance, float tasaAnual) {
+    return (balance * (tasaAnual / 12));
 
 }
+
+std::string CalculoPrestamo::reporteCalculoPrestamo(std::string tiempoTXT, std::string porcentajeTXT) {
+    std::string reporte = "";
+    int meses = calcularTiempoMeses(tiempoTXT) + 1;
+    float por = obtenerPorcentaje(porcentajeTXT);
+    float balance = getMonto();
+    float balanceInicial = getMonto();
+
+    for (int m = 1; m < meses; ++m) {
+        float interesMensual = obtenerInteresMensual(balanceInicial, por);
+        balance = balanceInicial;
+        balanceInicial = balance + interesMensual;
+
+        reporte = reporte + "Tasa[" + porcentajeTXT + "],Mes[" + std::to_string(meses) + "],balance inicial[" + std::to_string(balance) + "],interes[" + std::to_string(interesMensual) + "],balance nuevo[" + std::to_string(balanceInicial) + "] \n";
+
+    }
+
+    return reporte;
+}
+
 float CalculoPrestamo::getMonto() {
+    return monto;
 }
 
 void CalculoPrestamo::setMonto(float monto) {
-
+    this->monto = monto;
 }
 
 
